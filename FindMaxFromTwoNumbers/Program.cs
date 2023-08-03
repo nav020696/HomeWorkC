14cfaaa6d48e325b434b6edfc6d854c80a08cf2d
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-03 22:09:35 +0400

    Добавление первой задачи в проект по ДЗ

---
+++ FindMaxFromTwoNumbers/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите первое число");
+int numberFirst = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = int.Parse(Console.ReadLine());
+
+Console.WriteLine(numberFirst > numberSecond ? "Максимальное число первое: " + numberFirst : "Максимальное число второе: " + numberSecond);
