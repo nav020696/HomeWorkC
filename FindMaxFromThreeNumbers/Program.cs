ac4218eb75ad27da28aa67897995367ebc20b140
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-04 17:27:15 +0400

    Добавлено решение второй задачи

---
+++ FindMaxFromThreeNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int numberFirst = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberSecond = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberThird = int.Parse(Console.ReadLine());
+
+int max = numberFirst;
+if (numberSecond > max)
+{
+    max = numberSecond;
+}
+if (numberThird > max)
+{
+    max = numberThird;
+}
+
+Console.WriteLine("Максимальное число: " + max);
