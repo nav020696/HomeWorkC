7bea049a054efcc9da5226d8d4f1f9feff23fcf5
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-04 17:57:03 +0400

    Внесены корректировки

--- FindMaxFromThreeNumbers/Program.cs
+++ FindMaxFromThreeNumbers/Program.cs
@@ -4,12 +4,13 @@ Console.WriteLine("Введите второе число");
 int numberSecond = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int numberThird = int.Parse(Console.ReadLine());
-
 int max = numberFirst;
+
 if (numberSecond > max)
 {
     max = numberSecond;
 }
+
 if (numberThird > max)
 {
     max = numberThird;
