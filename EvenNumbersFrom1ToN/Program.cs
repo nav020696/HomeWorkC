e9dcb51fb6b31424444d37bc0c68cbdc7fb70e81
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-04 17:52:12 +0400

    Добавлено 4 задание

---
+++ EvenNumbersFrom1ToN/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int index = 1;
+
+if (number <= index)
+{
+    Console.WriteLine("Задайте число больше 1");
+}
+
+while(index <= number)
+{
+    if (index % 2 == 0)
+    {
+        Console.Write(index + " ");
+    }
+    index++;
+}
