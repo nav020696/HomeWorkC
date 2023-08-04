20acafb776db929e02410ed0ee77e2aa5e16e28b
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-04 17:44:43 +0400

    Добавлено решение третьей задачи

---
+++ EvenOddNumber/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(number % 2 == 0 ? "Число " + number + " четное" : "Число " + number + " нечетное");
