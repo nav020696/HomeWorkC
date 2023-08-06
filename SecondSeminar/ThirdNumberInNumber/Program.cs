3224cac302640d7bbd6c2a15851d1030b2611cb3
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-06 16:17:29 +0400

    Добавлено решение 2 задачи 2 семинара

--- SecondSeminar/ThirdNumberInNumber/Program.cs
+++ SecondSeminar/ThirdNumberInNumber/Program.cs
@@ -1,14 +1,19 @@
-﻿int number = 233;
+﻿int number = new Random().Next(-1000000, 1000001);
+Console.WriteLine($"Введенное число: {number}");
 
 if (number > -100 && number < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
-else if ((number >= -100 && number <= -999) || (number >= 100 && number <= 999))
-{
-    Console.WriteLine($"Третья цифра в числе: {number % 10}");
-} 
 else
 {
-    
+    if (number < 0)
+    {
+        number = number * -1;
+    }
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра в числе: {number % 10}");
 }
