b10ba4e17cc54bc1f5e6f0d69fe3854d4409d8a1
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-08-06 15:39:46 +0400

    Добавлено финальное решение 3 задачи и каркас 2 задачи 2 семинара

---
+++ SecondSeminar/IsTheDayWeekend/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = new Random().Next(1,8);
+
+switch (number)
+{   
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine($"{number} день недели не выходной");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine($"{number} день недели выходной");
+        break;
+    default:
+        Console.WriteLine("Нет дня недели с таким номером");
+        break;
+}
---
+++ SecondSeminar/ThirdNumberInNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = 233;
+
+if (number > -100 && number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if ((number >= -100 && number <= -999) || (number >= 100 && number <= 999))
+{
+    Console.WriteLine($"Третья цифра в числе: {number % 10}");
+} 
+else
+{
+    
+}
