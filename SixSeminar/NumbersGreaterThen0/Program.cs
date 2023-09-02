4f9ded89a77a1b61bc4d83248869b229b026b806
Анна Неумоина <anna_neumoina@MacBook-Air-Anna.local>
2023-09-02 17:49:47 +0400

    Добавлено решение задач 6 семинара

---
+++ SixSeminar/NumbersGreaterThen0/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Введите количество элементов");
+int count = int.Parse(Console.ReadLine());
+int[] newArray = GenerateArray(count);
+PrintArray(newArray);
+Console.WriteLine($"Количество элементов больше 0: {CountNumbersGreaterThen0(newArray)}");
+
+
+int[] GenerateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = GetMessage(i);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int i in array)
+    {
+        Console.Write($"{i} ");
+    }
+    Console.WriteLine();
+}
+
+int GetMessage(int index)
+{
+    Console.WriteLine($"Введите {index+1} элемент");
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNumbersGreaterThen0(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
---
+++ SixSeminar/PointOfIntersecionOfTwoLines/Program.cs
@@ -0,0 +1,61 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения k1, b1, k2 и b2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+//k1 * x + b1 = k2 * x + b2;
+//k1 * x - k2 * x = b2 - b1;
+// (k1-k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+
+Console.WriteLine ("Уравнение y = k * x + b");
+int[] newArray = GenerateArray();
+Print(newArray);
+PrintPoints(newArray);
+
+int[] GenerateArray()
+{
+    int[] array = new int[4];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = GetMessage(i);
+    }
+
+    return array;
+}
+
+void Print(int[] array)
+{
+    Console.WriteLine($"Первое уравнение: y = {array[0]} * x + {array[1]}");
+    Console.WriteLine($"Второе уравнение: y = {array[2]} * x + {array[3]}");
+}
+
+int GetMessage(int index)
+{
+    if (index == 1 || index == 3)
+    {
+        Console.WriteLine($"Введите b элемент");
+    }
+    else
+    {
+        Console.WriteLine($"Введите k элемент");
+    }
+    return int.Parse(Console.ReadLine());
+}
+
+int FindCoordinateX(int[] array)
+{
+    return (array[3] - array[1]) / (array[0] - array[2]);
+}
+
+int FindCoordinateY(int[] array)
+{
+    int x = FindCoordinateX(array);
+    return array[0] * x + array[1];
+}
+
+void PrintPoints(int[] array)
+{
+    Console.WriteLine($"Координаты пересечения двух прямых ({FindCoordinateX(array)} , {FindCoordinateY(array)})");
+}
