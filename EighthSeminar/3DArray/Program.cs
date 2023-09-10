c050849e765866bb6d357abe93349b1067791f0a
Lal007 <42681868+Lal007@users.noreply.github.com>
2023-09-10 23:13:38 +0300

    Добавлено решение 4 задачи

---
+++ EighthSeminar/3DArray/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+int[,,] array = GenerateArray(2,2,2);
+PrintArray(array);
+
+int[,,] GenerateArray(int rows, int column, int deep)
+{
+    Random random = new Random(); 
+    int[,,] array = new int[rows, column, deep];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = random.Next(100,1000);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    } 
+}
