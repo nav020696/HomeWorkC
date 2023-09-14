01298567ec548f1abe612ca972ba3a5b51ed9bcb
Lal007 <42681868+Lal007@users.noreply.github.com>
2023-09-14 18:26:39 +0300

    Добавлено решение 1 задачи

---
+++ NinthSeminar/FromNTo1/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(PrintNumbersFromNto1(number));
+
+string PrintNumbersFromNto1(int number)
+{
+    if (number == 1)
+    {
+        return number.ToString();
+    }
+    return number + " " + PrintNumbersFromNto1(number - 1);
+}
