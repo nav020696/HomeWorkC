27aecea48fcaac6ef2e52df67a8c40652c6e9e1c
Lal007 <42681868+Lal007@users.noreply.github.com>
2023-09-14 18:38:54 +0300

    Добавлено решение 2 задачи

---
+++ NinthSeminar/SumOfNumbersFromMToN/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine(SumOfNumbers(5,100));
+
+int SumOfNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return end;
+    }
+    return start + SumOfNumbers(start + 1, end);
+}
